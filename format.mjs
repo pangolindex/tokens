2743b6c8b25df96bf187cb9aadd4d17d5c27453f
dhrubabasu <7675102+dhrubabasu@users.noreply.github.com>
2021-08-03 13:14:26 -0400

    add formatting script

---
+++ format.mjs
@@ -0,0 +1,13 @@
+import {join} from 'node:path';
+import {renameSync, readdirSync} from 'node:fs';
+import {getAddress} from '@ethersproject/address';
+
+const ASSETS = './assets';
+
+const dirs = readdirSync(ASSETS).filter(name => name !== getAddress(name));
+
+if (dirs.length > 0) {
+	for (const name of dirs) {
+		renameSync(join(ASSETS, name), join(ASSETS, getAddress(name)));
+	}
+}
